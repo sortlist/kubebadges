92885fd57845e62f36ba1c82bf1d965a5cb37b3f
Thibault Gérondal <thibault@initia.cloud>
2024-12-25 22:52:22 +0100

    fix: Import GeneratorController in index_binding.dart

--- ui/lib/app/ui/pages/index/index_binding.dart
+++ ui/lib/app/ui/pages/index/index_binding.dart
@@ -2,6 +2,7 @@ import 'package:get/get.dart';
 import 'package:ui/app/controller/badge_controller.dart';
 import 'package:ui/app/controller/index_controller.dart';
 import 'package:ui/app/controller/settings_controller.dart';
+import 'package:ui/app/ui/pages/generator/generator_controller.dart';
 
 class IndexBinding implements Bindings {
   @override

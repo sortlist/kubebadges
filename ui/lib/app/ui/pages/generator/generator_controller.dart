26cdd326a36b316466272800bc339218a801f500
Thibault Gérondal <thibault@initia.cloud>
2024-12-25 22:08:31 +0100

    refactor: Translate comments to English and remove unnecessary comments

--- ui/lib/app/ui/pages/generator/generator_controller.dart
+++ ui/lib/app/ui/pages/generator/generator_controller.dart
@@ -2,12 +2,12 @@ import 'package:get/get.dart';
 import 'package:ui/app/model/model.dart';
 import 'package:ui/app/service/app_service.dart';
 
-// Ajout d'un simple "enum" pour les types supportés
+// Simple enum for supported types
 enum ResourceType {
   node,
   deployment,
   kustomization,
-  // on peut rajouter d'autres
+  // more can be added
 }
 
 class GeneratorController extends GetxController {
@@ -18,14 +18,14 @@ class GeneratorController extends GetxController {
   var resourceList = <KubeBadge>[].obs;
   var selectedItem = Rxn<KubeBadge>();
 
-  // Pour la liste des namespaces
+  // For the list of namespaces
   var namespaces = <KubeBadge>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     loadNamespaces();
-    loadResources(); // par défaut, node
+    loadResources(); // node by default
   }
 
   void loadNamespaces() async {
@@ -35,7 +35,7 @@ class GeneratorController extends GetxController {
     }
   }
 
-  // Charge la liste selon le type
+  // Load the list according to type
   void loadResources() async {
     resourceList.clear();
     selectedItem.value = null;
@@ -45,7 +45,7 @@ class GeneratorController extends GetxController {
         resourceList.assignAll(resp.body!);
       }
     } else if (resourceType.value == ResourceType.deployment) {
-      // il faut une namespace
+      // namespace required
       if (namespace.value.isEmpty) return;
       final resp = await appService.listDeployments(namespace.value, false);
       if (!resp.status.hasError) {

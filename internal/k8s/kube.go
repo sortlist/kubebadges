de8c260daab1d1f7d26397bfb4bf563b81ac4345
Thibault Gérondal <thibault@initia.cloud>
2024-12-31 17:32:43 +0100

    fix: Import missing batchv1 package in k8s kube.go

--- internal/k8s/kube.go
+++ internal/k8s/kube.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/kubebadges/kubebadges/pkg/generated/clientset/versioned"
+	batchv1 "k8s.io/api/batch/v1"
 	v1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
